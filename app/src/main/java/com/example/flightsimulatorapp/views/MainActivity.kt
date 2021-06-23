9aa1ae4c8f0db78a5e52759c1077a089433eb9fc
noamsal <noamsalomon01@gmail.com>
2021-06-23 23:32:05 +0300

    Noam - updated IP to accept '.', removed previous examples, changed app name a bit

--- app/src/main/java/com/example/flightsimulatorapp/views/MainActivity.kt
+++ app/src/main/java/com/example/flightsimulatorapp/views/MainActivity.kt
@@ -20,21 +20,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
+
+        //val rollButton: Button = findViewById(R.id.roll_button)
+
         //rollButton is of type 'Button'. now we're actually referring to a UI object!
         //now we're using rollButton to connect between him and the operating method for clicking him
         //p.s. - we're overriding this method (i guess)
-        rollButton.setOnClickListener { rollDice() }
+
+        //rollButton.setOnClickListener { rollDice() }
+
         /*register 'Submit' func. as a Handler for our connection button*/
         findViewById<Button>(R.id.Connect_Button).setOnClickListener { submit(it) } // 'it' refers to the submit button
     }
     // my addition
     //this function shall operate on a click - because SetOnClicked decided in 'onCreate'
-    private fun rollDice() {
-        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text) // get reference to 'TextView' GUI
-        resultText.text = "Dice Rolled" //change the 'TextView' 's text field to a different string
-    }
+//    private fun rollDice() {
+//        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+//        val resultText: TextView = findViewById(R.id.result_text) // get reference to 'TextView' GUI
+//        resultText.text = "Dice Rolled" //change the 'TextView' 's text field to a different string
+//    }
 
     //this function shall operate on a click on 'connection' button - because SetOnClicked decided in 'onCreate'
     private fun submit(Connect_Button : View) {   // the param. is the connection button itself
